a26051405750315cb5cdc9e7882ec0dd22b1d5a8
halex02 <halex02@msn.com>
2016-01-12 10:32:29 +0100

    blabla

--- TPs/TP01/tp1_ex2.pl
+++ TPs/TP01/tp1_ex2.pl
@@ -3,5 +3,5 @@ contenu(princesse).
 affiche1(princesse,tigre).
 affiche2(princesse,tigre).
 affiche2(tigre,princesse).
-salle2(X,Y):-affiche2(X,Y), not affiche1(X,Y).
-salle2(X,Y):-affiche1(X,Y), not affiche2(X,Y).
+salle2(X,Y):-affiche2(X,Y), not(affiche1(X,Y)).
+salle2(X,Y):-affiche1(X,Y), not(affiche2(X,Y)).
