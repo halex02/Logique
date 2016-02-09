7375ea264962599b3e02291cbafd221ec8f9ac14
halex02 <halex02@msn.com>
2016-02-09 11:55:42 +0100

    blabla

--- TPs/TP03/tp3.pl
+++ TPs/TP03/tp3.pl
@@ -31,5 +31,28 @@ verifie([X|L]) :- X ins 1..9, all_distinct(X), verifie(L), !.
 eclate([],[],[]).
 eclate([X|L1],[Y|L2],[[X|Y]|L3]):- eclate(L1, L2, L3).
 
+spliter([],[]).
+spliter([T|L],[[T]|R]) :- spliter(L,R).
+
+transp([],[]).
+transp([X],L) :- spliter(X,L).
+transp([X|L1],L) :- transp(L1,L2),eclate(X, L2, L).
+
+decoupe([],[],[],[]).
+decoupe([X1,Y1,Z1|L1],[X2,Y2,Z2|L2],[X3,Y3,Z3|L3],[[X1,Y1,Z1,X2,Y2,Z2,X3,Y3,Z3]|L]):- decoupe(L1,L2,L3,L).
+
+%%fonctionne pas renvois 3 carres pas 9 pour grille(L), carres(L,R), print(R).
+carres([],[]).
+carres([X,Y,Z],R):-decoupe(X,Y,Z,R).
+carres([_,_,_|L],R):- carres(L,R).
+
+
+%% fonctionne
+solution(G):- bonnetaille(G,9),verifie(G),transp(G,T),bonnetaille(T,9),verifie(T),carres(G,C),verifie(C).
+
+
+
+
+
 
 
